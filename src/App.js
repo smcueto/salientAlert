b1253b30e75e769db6082a115d5f8838f14f3737
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-06-01 19:05:01 -0700

    adding map component and rendering on main home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
+import './App.css';
 import CreateComponent from './components/CreateComponent';
 import EditComponent from './components/EditComponent';
 import IndexComponent from './components/IndexComponent';
+import MapDisplay from './components/MapDisplay';
 
 class App extends Component {
   render() {
     return (
     <Router>
+      <div>
        <div className="container">
         <nav className="navbar navbar-expand-md navbar-light bg-light">
             <a className="navbar-brand">Salient Alert</a>
@@ -27,11 +29,15 @@ class App extends Component {
                 </li>
               </ul>
           </nav>
+          </div>
+          <div className="container">
           <Switch>
+              <Route exact path='/' component={MapDisplay} />
               <Route exact path='/create' component={CreateComponent} />
               <Route path='/edit/:id' component={EditComponent} />
               <Route path='/index' component={IndexComponent} />
           </Switch>
+          </div>
         </div>
       </Router>
     );
