811a5209d1874a188a7a41618a9e65bd1fa66d4e
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-06-14 09:27:47 -0700

    adding some css to pages

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,7 @@ export default class HomePage extends Component {
     render() {
         return(
             <div>
-            Hello from home page!
+            <h1><center>Hello, welcome to Salient Alert</center></h1>
             </div>
         );
     }
--- src/ResourcesPage.js
+++ src/ResourcesPage.js
@@ -6,7 +6,10 @@ export default class ResourcesPage extends Component {
     render() {
         return(
             <div>
-           resource page!
+                <center>
+                <h1>Welcome to the respurce page.</h1>
+           <h2>Please check out these resources.</h2>
+           </center>
             </div>
         );
     }
