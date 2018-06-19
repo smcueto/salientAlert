189b2f274c56b75848c26fc145595be0fb99826a
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-06-19 16:41:53 -0700

    refactoring error msg

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const PORT = 4200;
 const cors = require('cors');
 const config = require('./database/DB');
@@ -9,17 +11,19 @@ const iceRaidRoutes = require('./routes/IceServerRoutes');
 const checkPointRoutes = require('./routes/CheckServerRoutes');
 
 mongoose.connect(config.DB).then(
-    () => {console.log('Database is connected') },
-    err => { console.log('Can not connect to the database' +err)
-});
+  () => { console.log('Database is connected'); },
+  (err) => {
+    console.log(`Can not connect to the database${err}`);
+  },
+);
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/iceraids', iceRaidRoutes);
 app.use('/checkpoints', checkPointRoutes);
 
-app.listen(PORT, function(){
-  console.log('Server is running on Port: ',PORT);
+app.listen(PORT, () => {
+  console.log('Server is running on Port: ', PORT);
 });
