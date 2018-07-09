fa170d6cb1da7af4bb3945e1ac13eb3120d46520
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-07-09 11:52:34 -0700

    resolving target blank link for resources

--- client/src/ResourcesPage.js
+++ client/src/ResourcesPage.js
@@ -7,9 +7,9 @@ function ResourcesPage() {
       <h1>Resources:</h1>
       <br />
       <ul>
-        <li><a href="https://notifica.us/" target="_blank">Notifica</a><br /> "The first app to prepare you and your family against deportation."</li><br />
-        <li><a href="http://sfilen.org/blog/new-hotline-gives-san-francisco-residents-a-way-to-report-ice-immigration-raids-and-protect-neighbors/" target="_blank">SFILEN</a><br /> The San Francisco Immigrant Legal & Education Network (SFILEN) set up the hotline at (415) 200-1548 to give the community a one-stop shop to report ICE raids. </li> <br />
-        <li><a href="https://www.aclu.org/know-your-rights/what-do-if-immigration-agents-ice-are-your-door" target="_blank">ACLU</a> <br /> KNOW YOUR RIGHTS: WHAT TO DO IF IMMIGRATION AGENTS (ICE) ARE AT YOUR DOOR</li>
+        <li><a href="https://notifica.us/" target="_blank" rel="noopener noreferrer">Notifica</a><br /> "The first app to prepare you and your family against deportation."</li><br />
+        <li><a href="http://sfilen.org/blog/new-hotline-gives-san-francisco-residents-a-way-to-report-ice-immigration-raids-and-protect-neighbors/" target="_blank" rel="noopener noreferrer">SFILEN</a><br /> The San Francisco Immigrant Legal & Education Network (SFILEN) set up the hotline at (415) 200-1548 to give the community a one-stop shop to report ICE raids. </li> <br />
+        <li><a href="https://www.aclu.org/know-your-rights/what-do-if-immigration-agents-ice-are-your-door" target="_blank" rel="noopener noreferrer">ACLU</a> <br /> KNOW YOUR RIGHTS: WHAT TO DO IF IMMIGRATION AGENTS (ICE) ARE AT YOUR DOOR</li>
       </ul>
     </div>
   );
