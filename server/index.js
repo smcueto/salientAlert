ba1fdc94f057cfec2f44bad79eec0e052d29278b
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-07-13 13:07:20 -0700

    changing port for heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const PORT = 4200;
+const PORT = process.env.PORT || 4200;
 const cors = require('cors');
 const config = require('./database/DB');
 const iceRaidRoutes = require('./routes/IceServerRoutes');
