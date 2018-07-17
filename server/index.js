20643cee511d878eef3039847d9801e1dac79e0a
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-07-17 15:05:38 -0700

    removing code for testing heroku deployment

--- server/index.js
+++ server/index.js
@@ -24,14 +24,6 @@ app.use(bodyParser.json());
 app.use('/iceraids', iceRaidRoutes);
 app.use('/checkpoints', checkPointRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
 app.listen(PORT, () => {
   console.log('Server is running on Port: ', PORT);
 });
