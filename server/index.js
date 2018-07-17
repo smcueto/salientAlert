1dbd80f17223c180b2c9f4dd9c2754711bb06361
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-07-17 14:51:46 -0700

    adding node path for deployment on heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT || 5000);
 const cors = require('cors');
 const config = require('./database/DB');
 const iceRaidRoutes = require('./routes/IceServerRoutes');
@@ -24,6 +24,14 @@ app.use(bodyParser.json());
 app.use('/iceraids', iceRaidRoutes);
 app.use('/checkpoints', checkPointRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.listen(PORT, () => {
   console.log('Server is running on Port: ', PORT);
 });
